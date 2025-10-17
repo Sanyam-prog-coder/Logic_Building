0f0e3452587f144a9071179852c3550f1b907641
Sanyam-prog-coder <ravnesanyam@gmail.com>
2025-10-17 14:00:10 +0530

    Add files via upload

---
+++ program18.java
@@ -0,0 +1,14 @@
+// Worst Approach
+
+class program18
+{
+    public static void main(String A[])
+    {
+        // Sequance
+
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+}
---
+++ program25.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// Static function
+
+void Display()
+{
+    int icnt = 0;
+
+    for(icnt = 1; icnt<= 4; icnt++)
+    {
+        printf("Jay ganesh...\n");
+    }
+}
+
+// It should be int main
+int main()
+{
+   Display();
+
+   return 0;
+}
